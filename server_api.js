75ff9538309af6991ffbfe62430b3b7ebf94cc2f
Kate Adkins <kateadkins@kates-mbp.myfiosgateway.com>
2020-10-24 17:06:30 -0400

    Incremental changes

--- server_api.js
+++ server_api.js
@@ -1,3 +1,19 @@
-function getForRent() {
-    fetch()
+/*
+Fetches data about houses that are available to rent
+
+Parameters:
+city: The city to search in (e.g. "New York City")
+state: A two-letter code representing the state to search in (e.g. "NY")
+offset: A number representing what point in the list of results should be the start point for 
+    returning results (so, "return <numberOfResults> results starting at item number <offset>").
+    Default is 0, representing the beginning of the list of results
+numberOfResults: The number of results to get back from the search (default is 10)
+sort: How the search should be done (default is to search by relevance)
+
+Return Value:
+Type array
+An array of result objects
+*/
+function getForRent(city, state, offset=0, numberOfResults=10, sort="relevance") {
+    fetch(`./get?api=realtor&numResults=${numberOfResults}&city=${city}&state=${state}&offset=${offset}`)
 }

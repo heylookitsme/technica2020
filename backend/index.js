c201e0b7acc04e4e37ede8c3e761d1f0648557b9
Kate Adkins <kateadkins@kates-mbp.myfiosgateway.com>
2020-10-24 23:17:14 -0400

    Putting things together

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,6 @@ http.createServer((req, res) => {
                 res.end(data);
             }
         });
-
     } else if (req.url == "/about") {
         fs.readFile("./about.html", (err, data) => {
             if (err) {
@@ -41,18 +40,40 @@ http.createServer((req, res) => {
                 res.end(data);
             }
         });
+    } else if (req.url == "/server_api.js") {
+        fs.readFile("./server_api.js", (err, data) => {
+            if (err) {
+                log.error(`Error when reading server_api.js: ${err}`);
+                res.statusCode = 500;
+                res.end("500 Internal Server Error");
+            } else {
+                res.end(data);
+            }
+        });
+    } else if (req.url == "/style.css") {
+        fs.readFile("./style.css", (err, data) => {
+            if (err) {
+                log.error(`Error when reading style.css: ${err}`);
+                res.statusCode = 500;
+                res.end("500 Internal Server Error");
+            } else {
+                res.end(data);
+            }
+        });
     } else if (req.url.split("?")[0] == "/get") {
         let query_string = req.url.split("?")[1];
         let query_fields = query_string.split("&"); // "api=realtor&numResults=${numberOfResults}&city=${city}&state=${state}&offset=${offset}&sort=${sort}"
         let query_object = {}; // ["api=realtor", "numResults=${numberOfResults}", "city=${city}", "state=${state}", "offset=${offset}", "sort=${sort}"]
         query_fields.forEach((key_pair) => {
-            query_object[key_pair.split("=")[0]] = value.split("=")[1];
+            query_object[key_pair.split("=")[0]] = key_pair.split("=")[1];
         });
         if (query_object.hasOwnProperty("api") && query_object.hasOwnProperty("numResults") &&
             query_object.hasOwnProperty("city") && query_object.hasOwnProperty("state") &&
             query_object.hasOwnProperty("offset") && query_object.hasOwnProperty("sort")) {
+                console.log(1);
                 api.getForSale(query_object.city, query_object.state, query_object.numResults)
                 .then((response) => {
+                    console.log(2);
                     let cur_house; // this represents the current house in the results of the api call
                     let current_house; // this will be inserted into res_to_client
                     let res_to_client = {
@@ -63,7 +84,10 @@ http.createServer((req, res) => {
                         current_house = {};
                         cur_house = response.properties[i];
                         if (cur_house.listing_status == "active" && cur_house.prop_status == "for_rent") {
+                            current_house.house = `${cur_house.property_id}`;
                             current_house.location = `${cur_house.address.line}, ${cur_house.address.city} ${cur_house.address.state}`;
+                            current_house.pets = `Allowed:\nDogs: ${cur_house.client_display_flags.allows_dogs}, Cats: ${cur_house.client_display_flags.allows_cats}, Small Dogs: ${cur_house.client_display_flags.allows_dogs_small}, Large Dogs: ${cur_house.client_display_flags.allows_dogs_large}`;
+                            current_house.other = `Year Built: ${cur_house.year_built}, Property Type: ${cur_house.prop_type}, Beds: ${cur_house.beds}`;
                             res_to_client.results.append(cur_house);
                             house_index++;
                         }
@@ -73,6 +97,10 @@ http.createServer((req, res) => {
                 .catch((reason) => {
                     log.error(`Error when getting api results: ${reason}`);
                 })
+        } else {
+            log.debug(`Client request to /get had incorrect query string: ${query_fields}`);
+            res.statusCode = 401;
+            res.end("Fix your query string");
         }
     } else {
         res.statusCode = 404;
--- backend/queryapi.js
+++ backend/queryapi.js
@@ -2,10 +2,11 @@ var unirest = require("unirest");
 
 function getForSale(city, state_code, limit) {
 	return new Promise((resolve, reject) => { // make this asynchronous
+		console.log(0.1);
 		var rq = unirest("GET", "https://realtor.p.rapidapi.com/properties/v2/list-for-sale");
 		rq.query({
 			"price_max":"50000", //arbitrary, change later or add a slider later. the idea is there should be a max tho because 
-						//were looking for affordable housing 
+						//we're looking for affordable housing 
 			"sort": "price_low",
 			"city": city,
 			"state_code": state_code,
@@ -18,9 +19,11 @@ function getForSale(city, state_code, limit) {
 			"useQueryString": true
 		});
 		rq.end(function (rs) {
+			console.log("Inside rs callback");
 			if (rs.error) {
 				reject(new Error(rs.error)); // effectively throw an error
 			} else {
+				console.log(rs.body);
 				resolve(rs.body); // successfully return the received data
 			}
 		});

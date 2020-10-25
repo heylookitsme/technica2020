6d3142e8a1ae2559d73f9599359dfea8cef2b2b2
Kate Adkins <kateadkins@kates-mbp.myfiosgateway.com>
2020-10-24 23:45:49 -0400

    Changed a few things (it still doesn't work though)

--- backend/queryapi.js
+++ backend/queryapi.js
@@ -1,10 +1,36 @@
-var unirest = require("unirest");
-
 function getForSale(city, state_code, limit) {
+	//var unirest = require("unirest");
 	return new Promise((resolve, reject) => { // make this asynchronous
+		const config = require("../config.json");
+		var http = require("https");
+		var options = {
+			"method": "GET",
+			"hostname": "realtor.p.rapidapi.com",
+			"port": null,
+			"path": `/properties/v2/list-for-rent?sort=relevance&city=${city}&state_code=${state_code}&limit=${limit}&offset=0`,
+			"headers": {
+				"x-rapidapi-host": "realtor.p.rapidapi.com",
+				"x-rapidapi-key": config.realtorKey,
+				"useQueryString": true
+			}
+		};
+		var req = http.request(options, function (res) {
+			var chunks = [];
+			res.on("data", function (chunk) {
+				chunks.push(chunk);
+			});
+			res.on("end", function () {
+				var body = Buffer.concat(chunks);
+				resolve(body.toJSON());
+			});
+			res.on('error', (err) => {
+				reject(err);
+			});
+		});
+		req.end();
+		/*
 		console.log(0.1);
-		var rq = unirest("GET", "https://realtor.p.rapidapi.com/properties/v2/list-for-sale");
-		rq.query({
+		var rq = unirest.get("https://realtor.p.rapidapi.com/properties/v2/list-for-sale").query({
 			"price_max":"50000", //arbitrary, change later or add a slider later. the idea is there should be a max tho because 
 						//we're looking for affordable housing 
 			"sort": "price_low",
@@ -12,13 +38,11 @@ function getForSale(city, state_code, limit) {
 			"state_code": state_code,
 			"limit": limit,
 			"offset": "0"
-		});
-		rq.headers({
+		}).header({
 			"x-rapidapi-host": "realtor.p.rapidapi.com",
 			"x-rapidapi-key": process.env[realtor_api_key],
 			"useQueryString": true
-		});
-		rq.end(function (rs) {
+		}).end(function (rs) {
 			console.log("Inside rs callback");
 			if (rs.error) {
 				reject(new Error(rs.error)); // effectively throw an error
@@ -27,6 +51,7 @@ function getForSale(city, state_code, limit) {
 				resolve(rs.body); // successfully return the received data
 			}
 		});
+		*/
 	});
 
 	var unirest = require("unirest"); // this is unreachable
